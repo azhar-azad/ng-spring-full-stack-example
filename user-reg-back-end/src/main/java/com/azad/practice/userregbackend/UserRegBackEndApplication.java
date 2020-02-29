507bc819ef5849a926a9662978632a1d066634b0
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-02-29 11:35:37 +0600

    second last commit

---
+++ user-reg-back-end/src/main/java/com/azad/practice/userregbackend/UserRegBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.azad.practice.userregbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegBackEndApplication.class, args);
+	}
+
+}
---
+++ user-reg-back-end/src/test/java/com/azad/practice/userregbackend/UserRegBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.practice.userregbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
